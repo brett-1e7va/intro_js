d41db8b37cef6b8e7103b385987dda6919b824db
Brett Leyva <brett1e7va@hotmail.com>
2022-03-09 17:57:13 -0600

    first commit

---
+++ Ejercicios/Ejercicio1.js
@@ -0,0 +1,26 @@
+/*ejercicio 1
+var dato = Number(prompt("Ingresa un numero"))
+var operacion = dato * 2
+console.log(operacion)
+*/
+
+/*ejercicio 2
+var dato1 = Number(prompt("Ingresa un numero"))
+var dato2 = Number(prompt("Ingresa un numero"))
+var operacion = dato1 - dato2
+console.log(operacion)
+*/
+
+/*ejercicio 3
+var pi = 3.1416
+var radio = 5.3
+var operacion = pi * radio ** 2
+console.log(operacion)
+*/
+
+/*ejercicio 4
+var D = 8
+var d = 6
+var operacion = D * d / 2
+console.log(operacion) 
+*/
