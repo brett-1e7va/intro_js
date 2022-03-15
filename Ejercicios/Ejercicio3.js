a83a044af7d398b960d86964f778d8a99e4a9796
Brett Leyva <brett1e7va@hotmail.com>
2022-03-14 21:45:44 -0600

    ejercicios

---
+++ Ejercicios/Ejercicio3.js
@@ -0,0 +1,53 @@
+/*
+var numero = prompt("Ingresa un numero");
+if (numero %2 === 0){
+    console.log(numero + " es divisible entre dos");
+} else {
+    numero + " no es divisible entre dos"
+};
+*/ 
+
+/*
+var numero = prompt("Ingresa un numero");
+if (numero %2 === 0){
+    console.log (alert(numero + " es par"))
+} else {
+    alert(numero + " no es par")
+};
+*/
+
+/*
+var numero = prompt("Ingresa un numero");
+if (numero === "1000"){
+    console.log("Ganaste un premio!!!");
+} else {
+    "Lo sentimos, sigue participando"
+};
+*/
+
+/*
+var numero = prompt("Ingresa un numero");
+var numero1 = prompt("Ingresa otro numero");
+
+if(numero < numero1){
+
+    console.log (numero + " es menor");
+
+} else {
+
+    console.log(numero1 + " es menor")
+};
+*/
+
+var numer1 = prompt("Ingresa un numero");
+var numero2 = prompt("Ingresa otro numero");
+var numero3 = prompt("Ingresa otro numero");
+
+if(numero < numero1){
+
+    console.log (numero + " es menor");
+
+} else {
+
+    console.log(numero1 + " es menor")
+};
---
+++ Ejercicios/Ejercicio6.js
@@ -0,0 +1,20 @@
+
+const array3 = animals.concat(comingSoonAnimals);
+
+/* Acomodar de mayor a menor
+console.log (array3)
+
+var arr = [4,6,1,0,8,2];
+
+arr.sort(function(a, b){return a-b});
+*/
+
+/* Agraea un nuevo animal
+animals.push("cow")
+console.log(animals);
+*/
+
+/* Elimina elemento "eagle"
+animals.shift()
+console.log(animals)
+*/
